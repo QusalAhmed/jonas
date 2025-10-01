57ca1831599c6082c5fe50280a530d49b81721e4
Qusal <qusal15@student.sust.edu>
2025-10-01 06:21:56 +0600

    Init

--- app/posts/PostForm.tsx
+++ app/posts/PostForm.tsx
@@ -76,7 +76,6 @@ export default function ProfileForm() {
 
     // 2. Define a submit handler.
     function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values);
         handleSubmit(values).then((response) => {
             console.log(response);
             if (response.status === 200) {
